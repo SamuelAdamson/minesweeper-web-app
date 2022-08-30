e4727574e5287d42af4f0ac0e3944665537f0588
SamuelAdamson <adamson.samuel10@gmail.com>
2022-08-30 11:59:39 -0600

    Starting toggle buttons for mode

--- src/Game/components/ModeSelect/ModeSelect.tsx
+++ src/Game/components/ModeSelect/ModeSelect.tsx
@@ -1,4 +1,5 @@
-import { Container, Button, ButtonGroup } from 'react-bootstrap'
+import { useEffect, useState } from 'react';
+import { Container, ToggleButton, ToggleButtonGroup } from 'react-bootstrap'
 import { Mode } from '../type' 
 import styles from './ModeSelect.module.css'
 import cx from 'classnames';
@@ -10,20 +11,35 @@ type Props = {
 const modes: Array<Mode> = ['easy', 'medium', 'hard']
 
 export const ModeSelect = ({ mode }: Props) => {
+  const [currMode, setCurrMode] = useState<Mode>(mode);
+  const onNewMode = (newMode: Mode) => {
+    mode = newMode;
+    console.log(mode);
+    // setCurrMode(newMode);
+  }
+
+  useEffect(() => {
+    console.log("HERE");
+    setCurrMode(mode);
+  }, [mode])
+
   return (
     <Container fluid className={ styles.ModeSelect }>
-      <ButtonGroup size="lg" >
-        {modes.map((m: Mode) => (
-          <Button 
+      <ToggleButtonGroup type="radio" name="options" defaultValue={0} >
+        {modes.map((m: Mode, idx: Number) => (
+          <ToggleButton 
+            key={`button__${m}`}
+            id={idx}
             className={ 
               (m == mode) ? cx(styles.ModeButton, styles.Selected)
               : styles.ModeButton
             }
+            onClick={() => onNewMode(m)}
           >
             {m}
-          </Button>
+          </ToggleButton>
         ))}
-      </ButtonGroup>
+      </ToggleButtonGroup>
     </Container>
   )
 }
