dc341ce9f33d4a83d957c4738b3eb2f7a1b84b93
SamuelAdamson <adamson.samuel10@gmail.com>
2023-01-15 21:05:30 -0500

    Starting on visualizer

--- pages/visualize.tsx
+++ pages/visualize.tsx
@@ -1,4 +1,7 @@
 import type { NextPage } from 'next';
+import { useState } from 'react';
+// import { Visual } from '../src';
+import { Algorithm } from '../src/Game/components/type';
 
 const Visualize: NextPage = () => {
   return(
