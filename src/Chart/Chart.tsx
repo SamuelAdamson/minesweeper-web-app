c6cc48b980e211565aeffcb5dbcda909b1de100b
SamuelAdamson <adamson.samuel10@gmail.com>
2024-06-12 21:26:23 -0600

    Reorganized to split

--- src/Chart/Chart.tsx
+++ src/Chart/Chart.tsx
@@ -6,109 +6,76 @@ import { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 const ApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
 import { Container } from 'react-bootstrap';
+import cx from 'classnames';
 
 import { Algorithm, Cascade, Clear } from '../index';
 import styles from './Chart.module.css';
+
 
 type Props = {
   newCascade: Cascade | null;
 };
 
-type ChartType = 'bar' | 'radial';
-
-type RadialChartData = ApexNonAxisChartSeries;
-
-type BarChartData = {
-  [Algorithm.DFS]: ApexNonAxisChartSeries;
-  [Algorithm.BFS]: ApexNonAxisChartSeries;
-}
-
 // placeholder for no chart data
-const radialEmptyText: string = 'No available cascade data.';
 const barEmptyTextBFS: string = 'No available BFS cascade data.';
 const barEmptyTextDFS: string = 'No available DFS cascade data.';
 
 // chart options
-const radialChartOptions: ApexCharts.ApexOptions = {
-  chart: {
-    type: 'radialBar',
-    height: 350,
-    width: '100%',
-    toolbar: {
-      show: false,
-    },
-  },
-  dataLabels: {
-    enabled: false
-  },
-  labels: [
-    'DFS',
-    'BFS'
-  ],
-  legend: {
-    show: true,
-    position: 'top',
-    horizontalAlign: 'left',
-    fontSize: '16px',
-    fontFamily: 'Poppins'
-  },
-  plotOptions: {
-    radialBar: {
-      offsetY: 0,
-      startAngle: 0,
-      endAngle: 270,
-      hollow: {
-        margin: 5,
-        size: '30%',
-        background: 'transparent',
-        image: undefined,
-      },
-      barLabels: {
-        enabled: true,
-        useSeriesColors: true,
-        margin: 12,
-        fontSize: '16px',
-        fontFamily: 'Poppins',
-      },
-    }
-  },
-};
-
 const barChartOptions: ApexCharts.ApexOptions = {
 
 };
 
 export const Chart = ({ newCascade, } : Props) => {
-  const [chartType, setChartType] = useState<ChartType>('bar');
-  const [barChartData, setBarChartData] = useState<BarChartData>({ [Algorithm.DFS]: [], [Algorithm.BFS]: [], });
-  const [radialChartData, setRadialChartData] = useState<RadialChartData>([]);
+ const [dataDFS, setDataDFS] = useState<ApexNonAxisChartSeries>([]);
+ const [dataBFS, setDataBFS] = useState<ApexNonAxisChartSeries>([]);
 
   const resetChart = () => {
-    setBarChartData({ [Algorithm.DFS]: [], [Algorithm.BFS]: [], });
-    setRadialChartData([]);
+    setDataDFS([]);
+    setDataBFS([]);
   }
   
   useEffect(() => {
-    /* TODO */
+    
   }, [newCascade]);
 
   return (
     <Container fluid className={styles.chart}>
       <div className={styles.chartHeader}>
-        <div className={styles.clearWrapper}><Clear onClear={resetChart}/></div>
-        <div>
-
-        </div>
+        <Clear onClear={resetChart} />
       </div>
       <div className={styles.chartContainer}>
-        {
-          chartType == 'bar' ?
-            <div>
-
-            </div>
-          :
-            <div></div>
-        }
+        <div className={cx(styles.chartSeparator, styles.top)}>
+          {
+            dataDFS.length == 0 ?
+              <div className={styles.chartPlaceholder}>
+                <p>{barEmptyTextDFS}</p>
+              </div>
+            :
+              <ApexChart
+                options={barChartOptions}
+                series={dataDFS}
+                type="bar"
+                height={200}
+                width={'100%'}
+              />
+          }
+        </div>
+        <div className={styles.chartSeparator}>
+          {
+            dataBFS.length == 0 ?
+              <div className={styles.chartPlaceholder}>
+                <p>{barEmptyTextBFS}</p>
+              </div>
+            :
+              <ApexChart
+                options={barChartOptions}
+                series={dataBFS}
+                type="bar"
+                height={200}
+                width={'100%'}
+              />
+          }
+        </div>
       </div>
     </Container>
   );
